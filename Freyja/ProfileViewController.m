3628d11ec5015f9ee32c128cfae1740a6bb8993b
Jose Manuel Ramírez Martínez <jmramirez@motofan.com>
2014-11-18 20:19:37 +0100

    Added the ViewController's: SettingsViewController, EditProfileViewController, ProfileViewController.

---
+++ Freyja/ProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.m
+//  Freyja
+//
+//  Created by Jose Manuel Ramirez Martinez on 18/11/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@property (strong, nonatomic) IBOutlet UIImageView *profilePictureImageView;
+@property (strong, nonatomic) IBOutlet UILabel *locationLabel;
+@property (strong, nonatomic) IBOutlet UILabel *ageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *statusLabel;
+@property (strong, nonatomic) IBOutlet UILabel *tagLineLabel;
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Freyja/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Freyja
+//
+//  Created by Jose Manuel Ramirez Martinez on 18/11/14.
+//  Copyright (c) 2014 Jose Manuel Ramírez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
