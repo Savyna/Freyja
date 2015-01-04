13a9bbf03cf4e3dd4866caa399450ff753234b0c
Jose Manuel Ramírez Martínez <jmramirez@motofan.com>
2015-01-04 20:41:46 +0100

    Added avaliable Chats with a  picture to the Chat List

--- Freyja/MatchesViewController.m
+++ Freyja/MatchesViewController.m
@@ -7,18 +7,35 @@
 //
 
 #import "MatchesViewController.h"
+#import <Parse/Parse.h>
+#import "Constants.h"
 
-@interface MatchesViewController ()
+@interface MatchesViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (strong, nonatomic) NSMutableArray *avaliableChatRooms;
 
 @end
 
 @implementation MatchesViewController
 
+#pragma mark - Lazy instantiation
+
+-(NSMutableArray *)avaliableChatRooms
+{
+    if ( !_avaliableChatRooms ) {
+        _avaliableChatRooms = [[NSMutableArray alloc] init];
+    }
+    return _avaliableChatRooms;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.tableView.delegate     = self;
+    self.tableView.dataSource   = self;
+
+    [self updateAvailableChatRooms];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -35,5 +52,80 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+#pragma mark - Helper Methods
+
+- (void)updateAvailableChatRooms
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"ChatRoom"];
+    [query whereKey:@"user1" equalTo:[PFUser currentUser]];
+    
+    PFQuery *queryInverse = [PFQuery queryWithClassName:@"ChatRoom"];
+#warning - Not sure if query or queryInverse in next line
+    [query whereKey:@"user2" equalTo:[PFUser currentUser]];
+    
+    PFQuery *queryCombined = [PFQuery orQueryWithSubqueries:@[query, queryInverse]];
+    
+    [queryCombined includeKey:@"chat"];
+    [queryCombined includeKey:@"user1"];
+    [queryCombined includeKey:@"user2"];
+    
+    [queryCombined findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if ( !error ) {
+            [self.avaliableChatRooms removeAllObjects];
+            self.avaliableChatRooms = [objects mutableCopy];
+            [self.tableView reloadData];
+        }
+    }];
+}
+
+#pragma mark - UITableView DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.avaliableChatRooms count];
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell           = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    PFObject *chatRoom              = [self.avaliableChatRooms objectAtIndex:indexPath.row];
+    PFUser *likedUser;
+    PFUser *currentUser             = [PFUser currentUser];
+    PFUser *testUser1               = chatRoom[@"user1"];
+    
+    if ( [testUser1.objectId isEqual:currentUser.objectId] ) {
+        likedUser = [chatRoom objectForKey:@"user2"];
+    }
+    else {
+        likedUser = [chatRoom objectForKey:@"user1"];
+    }
+    
+    cell.textLabel.text = likedUser[@"profile"][@"firstName"];
+
+    // cell.imageView.image = placeholder image
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    PFQuery *queryForPhoto = [[PFQuery alloc] initWithClassName:@"Photo"];
+    [queryForPhoto whereKey:@"user" equalTo:likedUser];
+    
+    [queryForPhoto findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        
+        if ( [objects count] > 0 ) {
+            
+            PFObject *photo     = objects[0];
+            PFFile *pictureFile = photo[kPhotoPictureKey];
+            
+            [pictureFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                
+                cell.imageView.image        = [UIImage imageWithData:data];
+                cell.imageView.contentMode  = [UIViewContentModeScaleAspectFit];
+            }];
+        }
+    }];
+    
+    return cell;
+}
 
 @end
